b7249c3a4be7787a9c56b5b9a42be60b863a6dff
jzl1 <34795003+jzl1@users.noreply.github.com>
2019-11-09 12:36:39 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -132,7 +132,7 @@ create_vmess_URL_config() {
 		cat >/etc/v2ray/vmess_qr.json <<-EOF
 		{
 			"v": "2",
-			"ps": "233v2.com_${domain}",
+			"ps": "${domain}",
 			"add": "${domain}",
 			"port": "443",
 			"id": "${v2ray_id}",
